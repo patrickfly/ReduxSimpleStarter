b0c7893405928350b006121167318fa08a31e0d3
patrickfly <flypatrickg@163.com>
2016-08-11 23:37:32 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +11,7 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App/>
+  </Provider>,
+  document.querySelector('.container')
+);
